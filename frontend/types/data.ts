2fdd6adef93f3ee10bdae158a271f60a97bed9a8
Andrey Azov <andrey@ebi.ac.uk>
2026-04-15 10:08:28 +0100

    Update types

--- frontend/types/data.ts
+++ frontend/types/data.ts
@@ -1,15 +1,41 @@
 type SelectListFilterOption = {
   label: string;
   value: string;
-  is_selected_by_default: boolean;
 };
-
 
 type SelectListFilter = {
   id: string;
-  label: string;
+  type: 'checkbox_list',
+  title: string;
   options: SelectListFilterOption[];
 };
+
+type SelectFilter = {
+  id: string;
+  type: 'text',
+  title: string;
+  label: string; // how to label the input box (textarea)
+  hint?: string; // placeholder
+  help?: string; // help text
+};
+
+type RegexFilter = {
+  id: string;
+  type: 'regex',
+  title: string;
+  fields: [
+    {
+      id: string;
+      label: string;
+      hint?: string;
+      help?: string; // help text
+      // validating_regex: string;
+      // error_message: string;
+    }
+  ]
+};
+
+
 
 type Filter =
   | SelectListFilter;
