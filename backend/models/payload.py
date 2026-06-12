578ef89b674da814669f0bd21a5460ed49df46d1
Jon Keatley <jon@ebi.ac.uk>
2026-06-12 09:14:22 +0100

    Added payload

---
+++ backend/models/payload.py
@@ -0,0 +1,17 @@
+from typing import List, Literal, Optional
+from pydantic import BaseModel
+
+class OrderBy(BaseModel):
+    column_id: int
+    order: Literal["ASC","DESC"]
+
+class Filter(BaseModel):
+    filter_id: string
+    filter_value: string
+
+class Payload(BaseModel):
+    columns: Optional[int] = [1]
+    page: Optional[int] = 1
+    per_page: Optional[int] = 100
+    filters: Optional[List[Filter]] = []
+    order_by: Optional[OrderBy] = None
