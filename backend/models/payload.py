5df69d344a48992ac19ded2219bce70dc275c9aa
Jon Keatley <jon@ebi.ac.uk>
2026-06-15 15:11:36 +0100

    Corrected filters in payload

--- backend/models/payload.py
+++ backend/models/payload.py
@@ -6,12 +6,12 @@ class OrderBy(BaseModel):
     order: Literal["ASC","DESC"]
 
 class Filter(BaseModel):
-    filter_id: string
-    filter_value: string
+    filter_id: str
+    filter_value: str
 
 class Payload(BaseModel):
     columns: Optional[List[int]] = [1]
     page: Optional[int] = 1
     per_page: Optional[int] = 100
-    #filters: Optional[List[Filter]] = []
+    filters: Optional[List[Filter]] = []
     order_by: Optional[OrderBy] = None
